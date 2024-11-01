597671c3b63f6953acbbe8b2628b681c5bf17259
Aisheair <ankitkargupta98313@gmail.com>
2024-11-01 19:38:23 +0530

    ss

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+//variable = A comtainer that stores a valu. bhaves as if it were the value it contains.
+
+//1. declaration   let x;
+//2. assignment    x = 100;
+
+// let x;
+// x = 12300;
+
+// let x = 123;
+
+console.log(x);
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+console.log(`you are ${age} years old`);
+console.log(`the price is $${price}`);
+console.log(`your gpa is: ${gpa}`);
