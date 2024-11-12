cf99ffedbe4e2ab4caf2a64ddf4e9c393578f5e7
Aisheair <ankitkargupta98313@gmail.com>
2024-11-12 19:23:45 +0530

    SWITCH

--- main.js
+++ main.js
@@ -211,21 +211,92 @@
 //                      condition ? codeIfTrue : codeIfFalse;
 
 
-let age = 21
+// let age = 21
 
-let nes = age >= 18 ? "you are an adult" : "you are a minor";
+// let nes = age >= 18 ? "you are an adult" : "you are a minor";
 
-console.log(nes);
+// console.log(nes);
 
-let time = 16;
+// let time = 16;
  
-let greeting = time < 12 ? "good morning" : "good afternoon";
+// let greeting = time < 12 ? "good morning" : "good afternoon";
 
-console.log(greeting);
+// console.log(greeting);
 
-let isStudent = false;
-let message = isStudent ? "you are a student" : "you are not a student";
-console.log(message);
+// let isStudent = false;
+// let message = isStudent ? "you are a student" : "you are not a student";
+// console.log(message);
+
+// SWITCH = can be an efficient replacement to many if statement
+
+let day = 7;
+
+switch(day){
+    case 1:
+        console.log("it is monday");
+        break;
+    case 2:
+        console.log("it is Tuesday");
+        break;
+    case 3:
+        console.log("it is Wednesday");
+        break;
+    case 4:
+        console.log("it is Thusday");
+        break;
+    case 5:
+        console.log("it is Friday");
+        break;
+    case 6:
+        console.log("it is Saturday");
+        break;
+    case 7:
+        console.log("it is Sunday");
+        break;
+    default:
+        console.log(`${day} is not a day`);
+        
+}
+
+let testScore = 66;
+let letterGrade;
+
+switch(true){
+    case testScore >= 90:
+        letterGrade = "A";
+        break;
+    case testScore >= 80:
+        letterGrade = "B";
+        break;
+    case testScore >= 70:
+        letterGrade = "C";
+        break;
+    case testScore >= 60:
+        letterGrade = "D";
+        break;
+    case testScore >= 50:
+        letterGrade = "E";
+        break;
+        default:
+            letterGrade = "F";
+}
+
+console.log(letterGrade);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
