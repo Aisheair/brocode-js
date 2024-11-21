628a3ab5ee291aa734a5cb6f514b4dd30f868f50
Aisheair <ankitkargupta98313@gmail.com>
2024-11-21 23:58:21 +0530

    filter()

--- main.js
+++ main.js
@@ -799,44 +799,83 @@
 //          to each element of an array, then return a new array
 
 
-const numbers = [1,2, 3, 4, 5]
+// const numbers = [1,2, 3, 4, 5]
 
-const squares = numbers.map(square)
-const cubes = numbers.map(cube)
-console.log(squares);
-console.log(cubes);
+// const squares = numbers.map(square)
+// const cubes = numbers.map(cube)
+// console.log(squares);
+// console.log(cubes);
 
 
-function square(element) {
-    return Math.pow(element, 2)
+// function square(element) {
+//     return Math.pow(element, 2)
+// }
+
+// function cube(element) {
+//     return Math.pow(element, 3)
+// }
+
+
+// const student = ["spongabob", "patrick", "squidwared", "sandy"]
+
+// const studentUpper = student.map(uppercase)
+// console.log(studentUpper);
+
+
+
+// function uppercase(element){
+//     return element.toUpperCase();
+// }
+
+
+// const dates = ["2024-1-10", "2025-2-20", "2026-3-30"]
+// const formattedDates = dates.map(formatDates)
+// console.log(formattedDates);
+
+
+// function formatDates(element){
+//     const parts = element.split("-");
+//     return `${parts[1]}/${parts[2]}/${parts[0]}`
+// }
+
+
+// .filter() = creats a new array by filteing out elements
+
+let numbers = [1, 2, 3, 4, 5, 6, 7]
+
+
+let evenNums = numbers.filter(isEven)
+console.log(evenNums);
+
+let oddNum = numbers.filter(isodd);
+console.log(oddNum);
+
+
+
+function isEven(element){
+    return element % 2 === 0;
+}
+function isodd(element){
+    return element % 2 !== 0;
 }
 
-function cube(element) {
-    return Math.pow(element, 3)
+
+const ages = [16, 17, 18, 18, 19, 20, 60];
+const is18 = ages.filter(isAdult)
+console.log(is18);
+
+
+
+function isAdult(element) {
+    return element >= 18
 }
 
 
-const student = ["spongabob", "patrick", "squidwared", "sandy"]
-
-const studentUpper = student.map(uppercase)
-console.log(studentUpper);
 
 
 
-function uppercase(element){
-    return element.toUpperCase();
-}
 
 
-const dates = ["2024-1-10", "2025-2-20", "2026-3-30"]
-const formattedDates = dates.map(formatDates)
-console.log(formattedDates);
-
-
-function formatDates(element){
-    const parts = element.split("-");
-    return `${parts[1]}/${parts[2]}/${parts[0]}`
-}
 
 
 
